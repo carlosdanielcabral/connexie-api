df7b6b74df59aa54f18d3e331aee217238ba6150
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2025-01-05 23:16:54 -0300

    test: add tests to file repository

---
+++ src/infrastructure/database/repositories/file-repository.test.ts
@@ -0,0 +1,34 @@
+
+import { PrismaClient } from "@prisma/client";
+import Sinon from "sinon";
+import File from "../../../domain/entities/file";
+import FileRepository from "./file-repository";
+
+describe("[Repository] File", () => {
+    const file = new File('original-name', 'encoding', 'mimeType', 'blobName', 1, 0, 'url', 1);
+
+    const prisma = new PrismaClient();
+
+    describe("01. Create", () => {
+        test("Return file after success insertion", async () => {
+            Sinon.stub(prisma, 'file').value({
+                create: Sinon.stub().resolves({
+                    id: file.id,
+                    originalName: file.originalName,
+                    encoding: file.encoding,
+                    mimeType: file.mimeType,
+                    blobName: file.blobName,
+                    originalSize: file.originalSize,
+                    compressedSize: file.compressedSize,
+                    url: file.url,
+                }),
+            });
+
+            const repository = new FileRepository(prisma);
+
+            const response = await repository.create(file);
+
+            expect(response).toBe(file);
+        });
+    });
+})
