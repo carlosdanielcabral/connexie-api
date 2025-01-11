a1498e82ee4b9d3bb4c30491813dbbbb0a27fa72
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2025-01-11 10:33:57 -0300

    feat: register address route

--- src/infrastructure/http/router.ts
+++ src/infrastructure/http/router.ts
@@ -3,12 +3,14 @@ import Route from '../../interfaces/routes/route';
 import ServiceProviderRoute from './routes/service-provider-route';
 import FileRoute from './routes/file-route';
 import JobAreaRoute from './routes/job-area';
+import AddressRoute from './routes/address-route';
 
 class Router {
     constructor(
         private readonly _serviceProviderRoute: Route = new ServiceProviderRoute(),
         private readonly _fileRoute: Route = new FileRoute(),
         private readonly _jobAreaRoute: Route = new JobAreaRoute(),
+        private readonly _addressRoute: Route = new AddressRoute(),
     ) {
     }
 
@@ -18,6 +20,7 @@ class Router {
         this._serviceProviderRoute.register(app);
         this._fileRoute.register(app);
         this._jobAreaRoute.register(app);
+        this._addressRoute.register(app);
     }
 }
 
